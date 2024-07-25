7bb20b1518e4a6dcdfc993094b0592c630e18b99
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-07-25 08:54:27 -0600

    Created ase HTML layout and added base styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import { toDate, format } from "date-fns";
+import "./assets/css/styles.css";
+import { createTodo } from "./todos";
+import { createProject } from "./projects";
 
-console.log(format(Date.now(), "PP"));
+let defaultProject = createProject("Home");
+
+defaultProject.addTodo(
+  createTodo(
+    "Get familiar with the app",
+    "Explore the app and see what it has to offer, start adding todos"
+  )
+);
+
+// let todoList = defaultProject.getTodoList();
+
+// todoList.forEach((todo, i) => {
+//   console.log(todo.getTitle());
+//   console.log(todo.getDescription());
+//   console.log(todo.getPriority());
+// });
+
+// todoList[0].setPriority("LO");
+
+// todoList.forEach((todo, i) => {
+//   console.log(todo.getTitle());
+//   console.log(todo.getDescription());
+//   console.log(todo.getPriority());
+// });
+
+// TODO: Create module that interacts with UI. Create displayHandler/screenHandler to handle interaction between logic and UI modules
