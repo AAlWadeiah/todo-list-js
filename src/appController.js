9b47b53aef7ac92620c60935c078ccddf7b358e7
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-07-27 11:36:05 -0600

    Added button to add todos and created small form with cancel and submit buttons. Configured event listener to show form

--- src/appController.js
+++ src/appController.js
@@ -1,4 +1,9 @@
-import { updateProjectList, updateProjectDetails } from "./appView";
+import {
+  updateProjectList,
+  updateProjectDetails,
+  drawAddTodo,
+  drawTodoForm,
+} from "./appView";
 import { createTodo } from "./todos";
 import { createProject } from "./projects";
 import { startOfToday, format } from "date-fns";
@@ -12,7 +17,13 @@ function projectClickHandler(e) {
 }
 
 function projectDetailsClickHandler(e) {
-  console.log(e.target.dataset.id);
+  switch (e.target.id) {
+    case "add-todo-btn":
+      e.target.classList.toggle("hidden");
+      drawTodoForm(e.target.closest("#project-details"));
+    default:
+      break;
+  }
 }
 
 function createDefaultProject() {
@@ -38,8 +49,13 @@ export const screenController = (function (
   createDefaultProject();
   updateProjectList(projectListContainer, projectList);
   updateProjectDetails(projectDetailsContainer, projectList[0]);
+  drawAddTodo(projectDetailsContainer);
 
   projectListContainer.addEventListener("click", projectClickHandler);
 
-  projectDetailsContainer.addEventListener("clicl", projectDetailsClickHandler);
+  projectDetailsContainer.addEventListener("click", projectDetailsClickHandler);
 })(projListContainer, projDetailsContainer);
+
+// TODO: need to add event listner for Todo form.
+//      Configure behavior when submit button is clicked: create new Todo, hide form, update todo list and show "Add a todo" button
+//      Behavior when cancel button is clicked: hide form and show same todo list and "Add a todo" button
--- src/appView.js
+++ src/appView.js
@@ -40,3 +40,44 @@ export function updateProjectDetails(container, project) {
     container.appendChild(todoDiv);
   }
 }
+
+export function drawAddTodo(container) {
+  const addTodoBtn = document.createElement("button");
+  addTodoBtn.id = "add-todo-btn";
+  addTodoBtn.classList.toggle("tertiary-cta-btn");
+  addTodoBtn.classList.toggle("active");
+  addTodoBtn.textContent = "+ Add a todo";
+  container.appendChild(addTodoBtn);
+}
+
+export function drawTodoForm(container) {
+  const formContainer = document.createElement("div");
+  formContainer.classList.toggle("form-container");
+  formContainer.classList.toggle("active");
+
+  const form = document.createElement("form");
+  form.action = "";
+  form.method = "get";
+  form.id = "todo-form";
+
+  const titleInput = document.createElement("input");
+  titleInput.type = "text";
+  titleInput.name = "todo-title";
+  titleInput.id = "todo-title";
+  titleInput.placeholder = "Title";
+  titleInput.required = true;
+
+  const cancelBtn = document.createElement("button");
+  cancelBtn.classList.toggle("secondary-cta-btn");
+  cancelBtn.textContent = "Cancel";
+  cancelBtn.value = "cancel";
+
+  const addTodoBtn = document.createElement("button");
+  addTodoBtn.classList.toggle("main-cta-btn");
+  addTodoBtn.textContent = "Add todo";
+  addTodoBtn.type = "submit";
+
+  form.append(titleInput, cancelBtn, addTodoBtn);
+  formContainer.appendChild(form);
+  container.appendChild(formContainer);
+}
