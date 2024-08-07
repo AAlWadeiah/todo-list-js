d373fc6116a2877243d5a1414f55f3798d482e1a
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2024-08-07 17:08:34 -0600

    Added functionality for default value when generating new todo form

--- src/appView.js
+++ src/appView.js
@@ -1,16 +1,20 @@
 import { getPriorityColorClass } from "./todos";
+import moment from "moment";
+import { format } from "date-fns";
 
 function clearContainer(container) {
   container.innerHTML = "";
 }
 
-export function isVisible(elem) {
-  // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
-  return (
-    !!elem &&
-    !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length)
-  );
-}
+// DELETE IF NOT USED
+// export function isVisible(elem) {
+//   // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+//   return (
+//     !!elem &&
+//     !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length)
+//   );
+// }
+
 export function updateProjectList(container, projects) {
   for (let proj of projects) {
     const pDiv = document.createElement("div");
@@ -21,6 +25,14 @@ export function updateProjectList(container, projects) {
 
     container.appendChild(pDiv);
   }
+}
+
+function drawEditTodo(container) {
+  const editTodoBtn = document.createElement("button");
+  editTodoBtn.classList.toggle("tertiary-cta-btn");
+  editTodoBtn.classList.toggle("edit-btn");
+  editTodoBtn.textContent = "Edit";
+  container.appendChild(editTodoBtn);
 }
 
 export function updateProjectDetails(container, project) {
@@ -44,12 +56,12 @@ export function updateProjectDetails(container, project) {
     const todoContent = document.createElement("div");
     todoContent.classList.toggle("todo-content");
 
-    const todoTitle = document.createElement("p");
+    const todoTitle = document.createElement("div");
     todoTitle.textContent = todo.getTitle();
     todoTitle.classList.toggle("todo-title");
     todoTitle.classList.toggle("todo-field");
 
-    const todoDesc = document.createElement("p");
+    const todoDesc = document.createElement("div");
     todoDesc.textContent = todo.getDescription();
     todoDesc.classList.toggle("subtext");
     todoDesc.classList.toggle("todo-desc");
@@ -87,6 +99,7 @@ export function updateProjectDetails(container, project) {
     }
 
     todoContent.append(todoTitle, todoDesc, bottomDiv);
+    drawEditTodo(todoContent);
     todoContainer.appendChild(todoContent);
     container.appendChild(todoContainer);
   }
@@ -100,7 +113,7 @@ export function drawAddTodo(container) {
   container.appendChild(addTodoBtn);
 }
 
-export function generateTitleInput() {
+export function generateTitleInput(defVal = null) {
   const titleInput = document.createElement("input");
   titleInput.type = "text";
   titleInput.name = "todo-title";
@@ -110,10 +123,11 @@ export function generateTitleInput() {
   titleInput.focus();
   titleInput.style.gridArea = "title";
   titleInput.classList.toggle("form-control");
+  if (defVal) titleInput.value = defVal;
   return titleInput;
 }
 
-export function generateDescInput() {
+export function generateDescInput(defVal = null) {
   const descInput = document.createElement("input");
   descInput.type = "text";
   descInput.name = "todo-desc";
@@ -121,28 +135,35 @@ export function generateDescInput() {
   descInput.placeholder = "Description";
   descInput.style.gridArea = "desc";
   descInput.classList.toggle("form-control");
+  if (defVal) descInput.value = defVal;
   return descInput;
 }
 
-export function generateDueDateInput() {
+export function generateDueDateInput(defVal = null) {
   const dueDate = document.createElement("input");
   dueDate.type = "date";
   dueDate.id = "todo-due-date";
   dueDate.style.gridArea = "due-date";
   dueDate.classList.toggle("form-control");
+  if (defVal) {
+    dueDate.value = moment(defVal).format("YYYY-MM-DD");
+  }
   return dueDate;
 }
 
-export function generateDueTimeInput() {
+export function generateDueTimeInput(defVal = null) {
   const dueTime = document.createElement("input");
   dueTime.type = "time";
   dueTime.id = "todo-due-time";
   dueTime.style.gridArea = "due-time";
   dueTime.classList.toggle("form-control");
+  if (defVal) {
+    dueTime.value = format(defVal, "hh:mm");
+  }
   return dueTime;
 }
 
-export function generatePrioritySelect() {
+export function generatePrioritySelect(defVal = null) {
   let priorityArr = ["Low", "Medium", "High"];
   const prioritySelect = document.createElement("select");
   prioritySelect.id = "todo-priority";
@@ -151,8 +172,14 @@ export function generatePrioritySelect() {
 
   const selectPlaceholder = document.createElement("option");
   selectPlaceholder.classList.toggle("select-placeholder");
-  selectPlaceholder.textContent = "Priority";
-  selectPlaceholder.value = "";
+  if (defVal) {
+    selectPlaceholder.value = defVal;
+    selectPlaceholder.textContent = defVal;
+  } else {
+    selectPlaceholder.textContent = "Priority";
+    selectPlaceholder.value = "";
+  }
+
   selectPlaceholder.disabled = true;
   selectPlaceholder.selected = true;
   selectPlaceholder.hidden = true;
@@ -168,7 +195,7 @@ export function generatePrioritySelect() {
   return prioritySelect;
 }
 
-export function drawTodoForm(container) {
+export function drawTodoForm(todoToEdit = null) {
   const formContainer = document.createElement("div");
   formContainer.classList.toggle("form-container");
 
@@ -177,57 +204,29 @@ export function drawTodoForm(container) {
   form.method = "get";
   form.id = "todo-form";
 
-  // const titleInput = document.createElement("input");
-  // titleInput.type = "text";
-  // titleInput.name = "todo-title";
-  // titleInput.id = "todo-title";
-  // titleInput.placeholder = "Title";
-  // titleInput.required = true;
-  // titleInput.focus();
-  // titleInput.style.gridArea = "title";
-  const titleInput = generateTitleInput();
+  let defTitle;
+  let defDesc;
+  let defDueDate;
+  let defDueTime;
+  let defPriority;
+  if (todoToEdit) {
+    const todoDetails = todoToEdit.getTodoDetails();
+    defTitle = todoDetails.title;
+    defDesc = todoDetails.description;
+    defDueDate = todoDetails.dueDate;
+    defDueTime = todoDetails.dueTime;
+    defPriority = todoDetails.priority;
+  }
 
-  // const descInput = document.createElement("input");
-  // descInput.type = "text";
-  // descInput.name = "todo-desc";
-  // descInput.id = "todo-desc";
-  // descInput.placeholder = "Description";
-  // descInput.style.gridArea = "desc";
-  const descInput = generateDescInput();
+  const titleInput = generateTitleInput(defTitle);
 
-  // const dueDate = document.createElement("input");
-  // dueDate.type = "date";
-  // dueDate.id = "todo-due-date";
-  // dueDate.style.gridArea = "due-date";
-  const dueDate = generateDueDateInput();
+  const descInput = generateDescInput(defDesc);
 
-  // const dueTime = document.createElement("input");
-  // dueTime.type = "time";
-  // dueTime.id = "todo-due-time";
-  // dueTime.style.gridArea = "due-time";
-  const dueTime = generateDueTimeInput();
+  const dueDate = generateDueDateInput(defDueDate);
 
-  // let priorityArr = ["Low", "Medium", "High"];
-  // const prioritySelect = document.createElement("select");
-  // prioritySelect.id = "todo-priority";
-  // prioritySelect.style.gridArea = "priority";
+  const dueTime = generateDueTimeInput(defDueTime);
 
-  // const selectPlaceholder = document.createElement("option");
-  // selectPlaceholder.classList.toggle("select-placeholder");
-  // selectPlaceholder.textContent = "Priority";
-  // selectPlaceholder.value = "";
-  // selectPlaceholder.disabled = true;
-  // selectPlaceholder.selected = true;
-  // selectPlaceholder.hidden = true;
-  // prioritySelect.appendChild(selectPlaceholder);
-
-  // for (let p of priorityArr) {
-  //   const option = document.createElement("option");
-  //   option.value = p;
-  //   option.textContent = p;
-  //   prioritySelect.appendChild(option);
-  // }
-  const prioritySelect = generatePrioritySelect();
+  const prioritySelect = generatePrioritySelect(defPriority);
 
   const cancelBtn = document.createElement("button");
   cancelBtn.classList.toggle("secondary-cta-btn");
@@ -253,7 +252,8 @@ export function drawTodoForm(container) {
     addTodoBtn
   );
   formContainer.appendChild(form);
-  container.appendChild(formContainer);
+  return formContainer;
+  // container.appendChild(formContainer);
 }
 
 export function hideElement(el) {
